c574750f504b94c2abdf147d7ee1351664c8f786
allen <getlose2011@gmail.com>
2016-05-18 18:16:44 +0800

    表單提交CSRF驗證及驗証碼判斷
    1. Input::all()//判斷是否表單送出
    2. {{csrf_field()}}//表單送出會判斷csrf
    3. back()->with('msg','驗證碼錯誤')//回上頁並給值要用session('msg')接收

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -5,26 +5,27 @@ namespace App\Http\Controllers\Admin;
 //use Illuminate\Http\Request;
 //use Illuminate\Support\Facades\DB;
 //use App\Http\Requests;
+use Illuminate\Support\Facades\Input;
+
 require_once ('resources/org/code/Code.class.php');//引入第三方的code
 
 class LoginController extends CommonController
 {
-    //
-
     public function login(){
-//        $pdo = DB::connection()->getPdo();
-//        dd($pdo);
-        return view('admin.login');
+        if($input = Input::all()){//判斷表單是否提交
+            $code = new \code;
+            $_code = $code->get();
+            if($input['code'] != $_code){
+                return back()->with('msg','驗證碼錯誤');//回上頁並給值要用session('msg')接收
+            }
+        }else{
+            return view('admin.login');
+        }
     }
 
     public function code(){
         $code = new \code;//從底層去方找
         echo $code->make();
-    }
-
-    public function getcode(){
-        $code = new \code;//從底層去方找
-        echo $code->get();
     }
 
 }
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -10,11 +10,15 @@
 		<h1>Blog</h1>
 		<h2>blog管理平台</h2>
 		<div class="form">
-			<p style="color:red">用戶名錯誤</p>
-			<form action="#" method="post">
+			@if(session('msg'))
+				<p style="color:red">{{session('msg')}}</p>
+			@endif
+			<form action="" method="post">
+				<!--表單送出會判斷csrf-->
+				{{csrf_field()}}
 				<ul>
 					<li>
-					<input type="text" name="username" class="text"/>
+					<input type="text" name="user_name" class="text"/>
 						<span><i class="fa fa-user"></i></span>
 					</li>
 					<li>
