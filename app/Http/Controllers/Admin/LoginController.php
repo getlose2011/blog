46cc9860c4902aec5e41cdb2ed7ffaa7c2348f3a
allen <getlose2011@gmail.com>
2016-05-19 13:26:47 +0800

    後台登入密碼Crypt加密及解密

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -6,6 +6,8 @@ namespace App\Http\Controllers\Admin;
 //use Illuminate\Support\Facades\DB;
 //use App\Http\Requests;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Crypt;
+use App\Http\Model\User;
 
 require_once ('resources/org/code/Code.class.php');//引入第三方的code
 
@@ -18,13 +20,20 @@ class LoginController extends CommonController
             if($input['code'] != $_code){
                 return back()->with('msg','驗證碼錯誤');//回上頁並給值要用session('msg')接收
             }
+            $user = User::first();//取到第一筆資料
+            if($user->user_name != $input['user_name'] || Crypt::decrypt($user->user_pass) != $input['password']){
+                return back()->with('msg','帳號或密碼有誤');//回上頁並給值要用session('msg')接收
+            }
+            session(['user' => $user]);
+            dd(session('user'));
         }else{
             return view('admin.login');
         }
     }
 
+    //圖片驗證碼
     public function code(){
-        $code = new \code;//從底層去方找
+        $code = new \code;
         echo $code->make();
     }
 
